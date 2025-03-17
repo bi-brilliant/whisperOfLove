678488818afbbf1f5029b3ab7555a11bd1ac92d8
bi-brilliant <bilaljibrilliant@gmail.com>
2025-03-17 22:03:12 +0700

    feat(UI): Separating between male and female profile

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -10,6 +10,7 @@ const MainContent: React.FC = () => {
     <div className="w-full h-screen flex flex-col md:flex-row items-center justify-between">
       <LeftSide images={["/image1.jpg", "/image2.jpg", "/image3.jpg"]} />
 
+      {/* Opening */}
       <div
         className="w-full md:w-1/3 h-full overflow-y-scroll snap-y snap-mandatory"
         style={{ scrollBehavior: "smooth" }}
@@ -36,6 +37,7 @@ const MainContent: React.FC = () => {
           </div>
         </motion.div>
 
+        {/* Quotes */}
         <motion.div
           className="relative h-screen flex flex-col items-center justify-end bg-center bg-cover snap-start pb-36"
           style={{ backgroundImage: 'url("/obj3.jpg")' }}
@@ -49,16 +51,107 @@ const MainContent: React.FC = () => {
           <div className="absolute inset-0 bg-gradient-to-t from-black/95 to-transparent pointer-events-none"></div>
         </motion.div>
 
+        {/* Bride */}
         <motion.div
-          className="relative h-screen flex flex-col items-center justify-end bg-center bg-cover snap-start gap-10 pb-32"
-          style={{ backgroundImage: 'url("/obj1.jpg")' }}
+          className="relative flex flex-col items-center justify-center bg-center bg-cover snap-start gap-10 pb-32 pt-32"
+          style={{ backgroundImage: 'url("/bg4.jpg")' }}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
         >
-          {/* Konten lainnya */}
+          <p className="text-lg text-center text-black px-8">
+            Dengan memohon Rahmat & Ridho Allah SWT, kami bermaksud mengundang
+            Bapak/Ibu/Saudara/i untuk menghadiri acara pernikahan kami:
+          </p>
+
+          {/* Man */}
+          {/* Pengantin Pria */}
+          <div className="flex flex-col items-center text-center px-8 py-12 w-4/5 rounded-lg shadow-lg bg-white/20 backdrop-blur-sm border border-white/30 space-y-6">
+            {/* Foto Pengantin */}
+            <div className="w-44 h-60 rounded-full overflow-hidden border-4 border-gold">
+              <img
+                src="/male1.jpg"
+                alt="Pengantin Pria"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            {/* Nama Pengantin */}
+            <motion.h2
+              className="text-2xl tracking-wider font-playfair text-black mt-6"
+              variants={{
+                hidden: { opacity: 0, y: 50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.3 }}
+            >
+              Andree Luhung Himawan, A.Md.Kes
+            </motion.h2>
+
+            {/* Deskripsi Orang Tua */}
+            <p className="text-sm text-black/80">
+              Putra Pertama dari Bapak Ahmad Rimbo Setiawan dan Ibu
+              Nirkumalasari
+            </p>
+
+            {/* Instagram */}
+            <a
+              href="https://instagram.com/Luhung_himawan"
+              target="_blank"
+              className="mt-5 font-semibold px-6 py-2 rounded-lg bg-black/20 hover:bg-black/40 transition duration-300 text-white hover:text-white"
+            >
+              @Luhung_himawan
+            </a>
+          </div>
+
+          {/* Separator */}
+          <div className="w-16 h-1 bg-gold rounded-full my-6"></div>
+
+          {/* Woman */}
+          <div className="flex flex-col items-center text-center px-8 py-12 w-4/5 rounded-lg shadow-lg bg-white/20 backdrop-blur-sm border border-white/30 space-y-6">
+            {/* Foto Pengantin */}
+            <div className="w-44 h-60 rounded-full overflow-hidden border-4 border-gold">
+              <img
+                src="/female1.jpg"
+                alt="Pengantin Pria"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            {/* Nama Pengantin */}
+            <motion.h2
+              className="text-2xl tracking-wider font-playfair text-black mt-6"
+              variants={{
+                hidden: { opacity: 0, y: 50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.3 }}
+            >
+              Andree Luhung Himawan, A.Md.Kes
+            </motion.h2>
+
+            {/* Deskripsi Orang Tua */}
+            <p className="text-sm text-black/80">
+              Putra Pertama dari Bapak Ahmad Rimbo Setiawan dan Ibu
+              Nirkumalasari
+            </p>
+
+            {/* Instagram */}
+            <a
+              href="https://instagram.com/Luhung_himawan"
+              target="_blank"
+              className="mt-5 font-semibold px-6 py-2 rounded-lg bg-black/20 hover:bg-black/40 transition duration-300 text-white hover:text-white"
+            >
+              @Luhung_himawan
+            </a>
+          </div>
         </motion.div>
 
+        {/* Gallery */}
         <motion.div
           className="relative flex flex-col items-center bg-center bg-cover snap-start"
           style={{ backgroundImage: 'url("/bg4.jpg")' }}
@@ -72,6 +165,7 @@ const MainContent: React.FC = () => {
           </div>
         </motion.div>
 
+        {/* Closer */}
         <motion.div
           className="relative h-screen flex flex-col items-center justify-center bg-center bg-cover snap-start"
           style={{ backgroundImage: 'url("/image3.jpg")' }}
