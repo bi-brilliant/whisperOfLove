e90c83bed865753e9607f627fadebe2272a35e0d
bi-brilliant <bilaljibrilliant@gmail.com>
2025-03-18 20:46:37 +0700

    refactor(UI): Separating Bride UI to component

---
+++ src/components/BrideSection.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const BrideSection: React.FC = () => {
+  return (
+    <motion.div
+      className="relative flex flex-col items-center justify-center bg-center bg-cover snap-start gap-10 pb-32 pt-24"
+      style={{ backgroundImage: 'url("/bg5.jpg")' }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <p className="text-md text-center text-black px-8 mb-10 font-poppins">
+        Dengan memohon Rahmat & Ridho Allah SWT, kami bermaksud mengundang
+        Bapak/Ibu/Saudara/i untuk menghadiri acara pernikahan kami:
+      </p>
+
+      {/* Groom */}
+      <div className="flex flex-col items-center text-center px-8 py-12 w-4/5 rounded-lg shadow-lg bg-white/20 backdrop-blur-sm border border-white/30 space-y-6">
+        <div className="w-44 h-60 rounded-full overflow-hidden border-4 border-gold">
+          <img
+            src="/male1.jpg"
+            alt="Pengantin Pria"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <motion.h2
+          className="text-2xl tracking-wider font-playfair text-black mt-6"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          Aldi Halimawan, A.Md.Kes
+        </motion.h2>
+        <p className="text-sm text-black/80">
+          Putra Pertama dari Bapak Ahmad Rimbo Setiawan dan Ibu Nirkumalasari
+        </p>
+        <a
+          href="https://instagram.com/bi_brilliant"
+          target="_blank"
+          className="mt-5 font-light text-xs px-3 py-1 rounded-lg bg-black/40 hover:bg-black/60 transition duration-300 text-white"
+        >
+          @aldi_halimawan
+        </a>
+      </div>
+
+      <img src="/icon_heart.png" className="h-40 object-cover" />
+
+      {/* Bride */}
+      <div className="flex flex-col items-center text-center px-8 py-12 w-4/5 rounded-lg shadow-lg bg-white/20 backdrop-blur-sm border border-white/30 space-y-6">
+        <div className="w-44 h-60 rounded-full overflow-hidden border-4 border-gold">
+          <img
+            src="/female1.jpg"
+            alt="Pengantin Wanita"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <motion.h2
+          className="text-2xl tracking-wider font-playfair text-black mt-6"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          Alina Syakira, S.M.
+        </motion.h2>
+        <p className="text-sm text-black/80">
+          Putri Kedua dari Bapak Fahmi Afwa dan Ibu Sartika Kasih
+        </p>
+        <a
+          href="https://instagram.com/bi_brilliant"
+          target="_blank"
+          className="mt-5 font-light text-xs px-3 py-1 rounded-lg bg-black/40 hover:bg-black/60 transition duration-300 text-white"
+        >
+          @aleena_syakira
+        </a>
+      </div>
+    </motion.div>
+  );
+};
+
+export default BrideSection;
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import HeaderSection from "./HeaderSection";
 import QuoteSection from "./QuoteSection";
 import GallerySection from "./GallerySection";
+import BrideSection from "./BrideSection";
 
 const MainContent: React.FC = () => {
   return (
@@ -52,103 +53,7 @@ const MainContent: React.FC = () => {
         </motion.div>
 
         {/* Bride */}
-        <motion.div
-          className="relative flex flex-col items-center justify-center bg-center bg-cover snap-start gap-10 pb-32 pt-24"
-          style={{ backgroundImage: 'url("/bg5.jpg")' }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-        >
-          <p className="text-md text-center text-black px-8 mb-10 font-poppins">
-            Dengan memohon Rahmat & Ridho Allah SWT, kami bermaksud mengundang
-            Bapak/Ibu/Saudara/i untuk menghadiri acara pernikahan kami:
-          </p>
-
-          {/* Man */}
-          {/* Pengantin Pria */}
-          <div className="flex flex-col items-center text-center px-8 py-12 w-4/5 rounded-lg shadow-lg bg-white/20 backdrop-blur-sm border border-white/30 space-y-6">
-            {/* Foto Pengantin */}
-            <div className="w-44 h-60 rounded-full overflow-hidden border-4 border-gold">
-              <img
-                src="/male1.jpg"
-                alt="Pengantin Pria"
-                className="w-full h-full object-cover"
-              />
-            </div>
-
-            {/* Nama Pengantin */}
-            <motion.h2
-              className="text-2xl tracking-wider font-playfair text-black mt-6"
-              variants={{
-                hidden: { opacity: 0, y: 50 },
-                visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.3 }}
-            >
-              Aldi Halimawan, A.Md.Kes
-            </motion.h2>
-
-            {/* Deskripsi Orang Tua */}
-            <p className="text-sm text-black/80">
-              Putra Pertama dari Bapak Ahmad Rimbo Setiawan dan Ibu
-              Nirkumalasari
-            </p>
-
-            {/* Instagram */}
-            <a
-              href="https://instagram.com/bi_brilliant"
-              target="_blank"
-              className="mt-5 font-light text-xs px-3 py-1 rounded-lg bg-black/40 hover:bg-black/60 transition duration-300 text-white hover:text-white"
-            >
-              @aldi_halimawan
-            </a>
-          </div>
-
-          {/* Separator */}
-          <img src="/icon_heart.png" className="h-40 object-cover" />
-
-          {/* Woman */}
-          <div className="flex flex-col items-center text-center px-8 py-12 w-4/5 rounded-lg shadow-lg bg-white/20 backdrop-blur-sm border border-white/30 space-y-6">
-            {/* Foto Pengantin */}
-            <div className="w-44 h-60 rounded-full overflow-hidden border-4 border-gold">
-              <img
-                src="/female1.jpg"
-                alt="Pengantin Pria"
-                className="w-full h-full object-cover"
-              />
-            </div>
-
-            {/* Nama Pengantin */}
-            <motion.h2
-              className="text-2xl tracking-wider font-playfair text-black mt-6"
-              variants={{
-                hidden: { opacity: 0, y: 50 },
-                visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.3 }}
-            >
-              Alina Syakira, S.M.
-            </motion.h2>
-
-            {/* Deskripsi Orang Tua */}
-            <p className="text-sm text-black/80">
-              Putri Kedua dari Bapak Fahmi Afwa dan Ibu Sartika Kasih
-            </p>
-
-            {/* Instagram */}
-            <a
-              href="https://instagram.com/bi_brilliant"
-              target="_blank"
-              className="mt-5 font-light text-xs px-3 py-1 rounded-lg bg-black/40 hover:bg-black/60 transition duration-300 text-white hover:text-white"
-            >
-              @aleena_syakira
-            </a>
-          </div>
-        </motion.div>
+        <BrideSection />
 
         {/* Gallery */}
         <motion.div
